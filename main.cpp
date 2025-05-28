565c9b1c2796876e45b259add218b92d86c8a52d
nasrullah878 <enasrullahkhan@gmail.com>
2025-05-28 18:10:37 +0500

    changes in main.cpp file

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    cout << "Hello from GitHub Actions!" << endl;
+    cout << "Hello from GitHub Actions! and changes in workflow fle for a while" << endl;
     return 0;
 }
 
