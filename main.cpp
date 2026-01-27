772d6d090c57d2b1e3f4ba1609b10272bfb1232c
nasrullah878 <enasrullahkhan@gmail.com>
2026-01-27 23:56:28 +0500

    nk changes

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    cout<<"3hello nasrulla khsn h";    
+    cout<<"32ello nasrulla khsn h";    
 cout << "Hello world from GitHub Actions! and changes in workflow fle for a while" << endl;
     return 0;
 }
